9f241a3fc89ba56cc852878b0358a97de6aabd23
최순영 <shelly@choesun-yeong-ui-MacBookPro.local>
2019-02-19 15:51:24 +0900

    refactoring picnic

--- ssun/chpt6/picnic.py
+++ ssun/chpt6/picnic.py
@@ -1,48 +1,40 @@
 
-test_case = int(input())
-pair = []
+def count_pairing(student_num, is_paired, pair):
 
-def contact_pair(index, has_mate):
-	global count
-	if check_countable():
-		count += 1
-	elif has_mate[index]:
-		contact_pair(index+1, has_mate)
+	if student_num == len(is_paired):
+		return 1
+
+	if is_paired[student_num]:
+		return count_pairing(student_num+1, is_paired, pair)
+
 	else:
-		for num in pair[index]:
-			if not has_mate[num]:
-				has_mate[num] = True
-				has_mate[index] = True
-				contact_pair(index + 1,has_mate)
-				has_mate[num] = False
-				has_mate[index] = False
+		res = 0
+		for student in pair[student_num]:
+			if not is_paired[student]:
+				is_paired[student] = True
+				is_paired[student_num] = True
+				res += count_pairing(student_num+1, is_paired, pair)
+				is_paired[student] = False
+				is_paired[student_num] = False
 
-				
-def check_countable():
-	for boolean in has_mate:
-		if not boolean:
-			return False
-	return True
-
-while test_case > 0:
-	global count
-	count = 0
-	student_num, pair_num = input().split(' ')
-	raw_pair = input().split(' ')
-
-	has_mate = [False for x in range(int(student_num))]
-	pair = [[] for x in range(int(student_num))]
-
-	for i in range(0, int(pair_num)):
-		first = int(raw_pair[i*2])
-		last = int(raw_pair[(i*2)+1])
-
-		pair[first].append(last)
-		pair[last].append(first)
+		return res
 
 
-	contact_pair(0, has_mate)
-	print(count)
 
-	test_case -= 1
+if __name__ == "__main__":
+	test_case = int(input())
 
+	while test_case > 0:
+		num_student, num_pair = map(int, input().split(' '))
+		pair = [[] for x in range(int(num_student))]
+		
+		inputed = [int(x) for x in input().split()]
+		is_paired = [False for i in range(0, num_student)]
+
+		for i in range(0, num_pair):
+			pair[inputed[i*2]].append(inputed[(i*2)+1])
+			pair[inputed[(i*2)+1]].append(inputed[i*2])
+
+		print(count_pairing(0, is_paired, pair))
+
+		test_case -= 1
