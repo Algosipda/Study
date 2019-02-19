7cf12e0348447e24205067f5d38f6d8febd4d489
최순영 <shelly@choesun-yeong-ui-MacBookPro.local>
2019-02-19 17:43:55 +0900

    edit boardcover

--- ssun/chpt6/boardcover.py
+++ ssun/chpt6/boardcover.py
@@ -1,62 +1,69 @@
+def counting_coverway(blocks_i, blocks_j, row_p, col_p, board):
 
-def covering_board(row_p, col_p, board, white_space):
-    global block_j, block_i
-    global count
+	if row_p >= len(board):
+		return 1
+	if col_p >= len(board[row_p]):
+		return counting_coverway(blocks_i, blocks_j,row_p+1, 0, board)
+	if board[row_p][col_p] == '#':
+		return counting_coverway(blocks_i, blocks_j, row_p, col_p + 1, board)
+	else:
+		res = 0
+		for i in range(0, len(blocks_i)):
+			if checking_coverable(blocks_i[i], blocks_j[i], row_p, col_p, board):
+				res += counting_coverway(blocks_i, blocks_j, row_p, col_p+1, board)
+				remove_block(blocks_i[i], blocks_j[i], row_p, col_p, board)
 
-    if white_space == 0:
-        count += 1
-    elif row_p >= len(board):
-        return
-    elif col_p >= len(board[row_p]):
-        return covering_board(row_p+1, 0, board, white_space)
-    elif board[row_p][col_p] == 1:
-        covering_board(row_p, col_p+1, board, white_space)
-    else:
-        for i in range(0, len(block_i)):
-            if set(board, row_p, col_p, 0, 1, block_i[i], block_j[i]):
-                covering_board(row_p, col_p+1, board, white_space - 3)
-            set(board, row_p, col_p, 0, -1, block_i[i], block_j[i])
+		return res
 
-def set(board, y, x, _type, delta, block_i, block_j):
-    ok = True
-    for i in range (0,3):
-        ny = y + block_i[i]
-        nx = x + block_j[i]
-        if ny < 0 or ny >= len(board) or nx < 0 or nx >= len(board[0]):
-            ok = False
-        else:
-            board[ny][nx] += delta
-            if(board[ny][nx] > 1):
-                ok = False
-    return ok
+def checking_coverable(block_i, block_j, row_p, col_p, board):
+	coverable = True
+
+	for cell_delta in range(0, len(block_i)):
+		if block_i[cell_delta] + row_p < 0 or block_i[cell_delta] + row_p >= len(board):
+			coverable = False
+			break
+		elif block_j[cell_delta] + col_p < 0. or block_j[cell_delta] + col_p >= len(board[row_p]):
+			coverable = False
+			break
+		elif board[row_p + block_i[cell_delta]][col_p + block_j[cell_delta]] == '#':
+			coverable = False
+			break
+
+	if coverable:
+		for cell_delta in range(0, len(block_j)):
+			board[row_p + block_i[cell_delta]][col_p + block_j[cell_delta]] = '#'
+
+
+	return coverable
+
+
+def remove_block(block_i, block_j, row_p, col_p, board):
+	for cell_delta in range(0, len(block_j)):
+			board[row_p + block_i[cell_delta]][col_p + block_j[cell_delta]] = '.'
 
 
 if __name__ == "__main__":
-    global block_j, block_i
-    global count
-    block_i = [[0, 1, 1], [0, 1, 1], [0, 0, 1], [0, 0, 1]]
-    block_j = [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, 1, 1]]
+	test_case = int(input())
+	blocks_i = [[0, 1, 1], [0, 0, 1], [0, 0, 1], [0, 1, 1]]
+	blocks_j = [[0, 0, 1], [0, 1, 1], [0, 1, 0], [0, 0, -1]]
 
-    test_case = int(input())
+	while test_case > 0:
+		board = []
+		row, col = map(int, input().split(' '))
+		white_space = 0
+		for i in range(0, row):
+			column = []
+			for cell in input():
+				if cell == '.':
+					white_space += 1
+				column.append(cell)
+			board.append(column)
 
-    while test_case > 0:
-        count = 0
-        initial_board = []
-        row, col = map(int, input().split(' '))
-        white_space = 0
-        for i in range(row):
-            column = []
-            for cell in input():
-                if cell == '.':
-                    column.append(0)
-                    white_space += 1
-                elif cell == '#':
-                    column.append(1)
-            initial_board.append(column)
-        if white_space % 3 != 0:
-            print(0)
-        else:
-            covering_board(0, 0, initial_board, white_space)
-            print(count)
+		if white_space % 3 !=0:
+			print(0)
+		else:
+			print(counting_coverway(blocks_i, blocks_j, 0, 0, board))
 
-        test_case -= 1
+		test_case -= 1
+
+
