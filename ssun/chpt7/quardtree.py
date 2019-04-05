32f957353cb37442ea0036652bb2984407cac47b
최순영 <shelly@choesun-yeong-ui-MacBookPro.local>
2019-04-05 12:40:47 +0900

    refactoring quard tree problem. 44ms->40ms

--- ssun/chpt7/quardtree.py
+++ ssun/chpt7/quardtree.py
@@ -1,27 +1,28 @@
-def reverse_picture(picture):
-    global index
-    reversed_picture = []
+def quard_tree(image):
+	if len(image) == 1:
+		return image
+	else:
+		pointer = 1
+		image_pices = []
+		for i in range(0, 4):
+			if not image[pointer] == 'x':
+				image_pices.append(image[pointer])
+				pointer += 1
+			else:
+				pice = quard_tree(image[pointer:])
+				image_pices.append(pice)
+				pointer += len(pice)
+		return 'x' + image_pices[2] + image_pices[3] + image_pices[0] + image_pices[1]
 
-    for i in range(0, 4):
-        if picture[index] == 'x':
-            index += 1
-            reversed_picture.append(reverse_picture(picture))
-        else:
-            reversed_picture.append(picture[index])
-            index += 1
-    return 'x'+reversed_picture[2] + reversed_picture[3] + reversed_picture[0]+reversed_picture[1]
 
+def main():
+	test_case = int(input())
+	while test_case > 0:
 
-if __name__ == "__main__":
-    test_case = int(input())
+		image = input()
+		print(quard_tree(image))
 
-    while test_case > 0:
-        global index
-        index = 0
-        picture = input()
-        if picture[index] == 'x':
-            index += 1
-            print(reverse_picture(picture))
-        else:
-            print(picture)
-        test_case -= 1
+		test_case -= 1
+
+if __name__ == '__main__':
+    main()
